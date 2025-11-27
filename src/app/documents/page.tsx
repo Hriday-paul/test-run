710516046a578e0f8d7e0ae294907944cc1b3628
Hriday-paul <hridoychandrapaul.10@gmail.com>
2025-11-27 12:02:43 +0600

    updated document top rout name

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -3,10 +3,10 @@
 function DocumentProcess() {
 
     return (
-        <div className="min-h-96 flex justify-center items-center">
+        <div className="min-h-96 flex flex-col justify-center items-center">
 
-            <h4 className="text-center text-black text-xl font-popin font-semibold mb-1">Page will come soon 🔜</h4>
-            <p className="text-base text-center text-gray-900 font-popin font-medium">Stay With Us 🤝</p>
+            <h4 className="text-center text-black text-xl lg:text-2xl font-popin font-semibold mb-1">Page will come soon 🔜</h4>
+            <p className="text-base lg:text-lg text-center text-gray-900 font-popin font-medium">Stay With Us 🤝</p>
 
         </div>
     )
