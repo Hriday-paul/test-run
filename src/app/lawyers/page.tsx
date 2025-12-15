02df3fa720abdd7fff82a4abe1768f221da4248d
Hriday-paul <hridoychandrapaul.10@gmail.com>
2025-12-15 22:14:45 +0600

    update lawyer ui

--- src/app/lawyers/page.tsx
+++ src/app/lawyers/page.tsx
@@ -17,7 +17,7 @@ function Lawyer() {
             </ShopBanner>
 
             <div className='bg-[#F2F4F8] py-8'>
-                <div className='col-span-1 md:col-span-2 lg:col-span-6 xl:col-span-3'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-8 xl:grid-cols-4 gap-5 container py-5'>
                     <div className='lg:col-span-2 xl:col-span-1 hidden lg:block'>
                         <LawyerFilter />
                     </div>
