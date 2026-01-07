38d0ddb0d27c12a30a5b76dccc7fd14d16f31003
Hriday-paul <hridoychandrapaul.10@gmail.com>
2026-01-07 23:17:02 +0600

    update root meta title & description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,13 +26,13 @@ export const metadata: Metadata = {
   },
   description: "Runbd providing best Car Buy/Sell, Bike Buy/Sell, Car Rent, Workshops, Accessories, Exchange, Lawyer, Job & Services — All in One Click. Skip the hassle and delays — easily buy, sell, or access essential services anytime from one reliable and secure platform.",
 
-  keywords : ["runbd", "Runbd", "রানবিডি", "রান", "car", "buy", "sell", "rent", "car rent", "car sell", "car buy", "bike buy", "bike sell", "bike", "bikes", "job", "jobs", "exchange", "workshop", "accessories", "exchange", "lawyer", "bangladesh cars", "used car", "used bike", "new car", "new bike", "bangladesh bike", "bangladesh workshop", "bangladesh lawyer", "গাড়ি", "নতুন গাড়ি", "পুরাতন গাডি", "গাড়ি বিক্রয়", "গাড়ি বিক্রি", "গাড়ি ক্রয়", "গাড়ি ভাড়া", "বাইক বিক্রয়", "বাইক বিক্রি", "বাইক ক্রয়", "মটরসাইকেল", "মটরসাইকেল বিক্রি", "মটরসাইকেল ক্রয়", "চাকরি", "ওয়ার্কশপ", "গাড়ির সরঞ্জাম", "উকিল", "বাইসাইকেল", "এক্সচেঞ্জ"],
+  keywords: ["runbd", "Runbd", "রানবিডি", "রান", "car", "buy", "sell", "rent", "car rent", "car sell", "car buy", "bike buy", "bike sell", "bike", "bikes", "job", "jobs", "exchange", "workshop", "accessories", "exchange", "lawyer", "bangladesh cars", "used car", "used bike", "new car", "new bike", "bangladesh bike", "bangladesh workshop", "bangladesh lawyer", "গাড়ি", "নতুন গাড়ি", "পুরাতন গাডি", "গাড়ি বিক্রয়", "গাড়ি বিক্রি", "গাড়ি ক্রয়", "গাড়ি ভাড়া", "বাইক বিক্রয়", "বাইক বিক্রি", "বাইক ক্রয়", "মটরসাইকেল", "মটরসাইকেল বিক্রি", "মটরসাইকেল ক্রয়", "চাকরি", "ওয়ার্কশপ", "গাড়ির সরঞ্জাম", "উকিল", "বাইসাইকেল", "এক্সচেঞ্জ"],
 
   metadataBase: new URL('https://runbd.org'),
 
   openGraph: {
-    title: 'Runbd providing best Buy, Sell & Services — All in One Click',
-    description: 'Runbd providing best Car Buy/Sell, Bike Buy/Sell, Car Rent, Workshops, Accessories, Exchange, Lawyer, Job & Services — All in One Click. Skip the hassle and delays — easily buy, sell, or access essential services anytime from one reliable and secure platform.',
+    title: 'Runbd - Buy/Sell & Services in Bangladesh',
+    description: 'Skip the hassle and delays — easily buy, sell, or access essential services anytime from one reliable and secure platform - Runbd.',
     url: 'https://runbd.org',
     siteName: 'Runbd',
     images: ["https://runbd.org/og-image.png"],
@@ -41,8 +41,8 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Runbd providing best Buy, Sell & Services — All in One Click',
-    description: 'Runbd providing best Car Buy/Sell, Bike Buy/Sell, Car Rent, Workshops, Accessories, Exchange, Lawyer, Job & Services — All in One Click. Skip the hassle and delays — easily buy, sell, or access essential services anytime from one reliable and secure platform.',
+    title: 'Runbd - Buy/Sell & Services in Bangladesh',
+    description: 'Skip the hassle and delays — easily buy, sell, or access essential services anytime from one reliable and secure platform - Runbd.',
     creator: '@runbd',
     images: ['https://runbd.org/og-image.png'],
   },
