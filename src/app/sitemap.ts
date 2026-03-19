e739b3079f4c0ed16435ad3ab19c39721ff988a5
Hriday paul <hriday@sparktechagency.com>
2026-03-19 08:29:35 +0600

    updated sitemap path

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,43 +3,43 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://runbd.com',
+            url: 'https://runbd.org',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
         },
         {
-            url: 'https://runbd.com/carbuysell',
+            url: 'https://runbd.org/carbuysell',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.97,
         },
         {
-            url: 'https://runbd.com/bikebuysell',
+            url: 'https://runbd.org/bikebuysell',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.95,
         },
         {
-            url: 'https://runbd.com/login',
+            url: 'https://runbd.org/login',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.90,
         },
         {
-            url: 'https://runbd.com/about',
+            url: 'https://runbd.org/about',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.85,
         },
         {
-            url: 'https://runbd.com/vehicle-process',
+            url: 'https://runbd.org/vehicle-process',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.8,
         },
         {
-            url: 'https://runbd.com/documents',
+            url: 'https://runbd.org/documents',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.7,
